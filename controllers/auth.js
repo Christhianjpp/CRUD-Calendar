962944b4577950bf57a2ef54c4d4e374428293c0
Christhian Peñalba <christhianjpp@gmail.com>
2022-03-29 10:47:29 -0500

    Auth

---
+++ controllers/auth.js
@@ -0,0 +1,119 @@
+const { response } = require('express')
+const bcrypt = require('bcryptjs');
+
+const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
+
+
+const crearUsuario = async (req, res = response) => {
+    const { email, password } = req.body
+
+    try {
+        // verifica si existe el correo en la bd
+        let usuario = await Usuario.findOne({ email })
+
+        if (usuario) {  // true si existe
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese correo'
+            })
+        }
+        // envia los datos del usuario al Schema
+        usuario = new Usuario(req.body)
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync()
+        usuario.password = bcrypt.hashSync(password, salt)
+
+        // Guarda el usuario en la base de datos
+        await usuario.save();
+
+        // Generar JWT
+        const token = await generarJWT(usuario.id, usuario.name)
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
+}
+
+
+const loginUsuario = async (req, res = response) => {
+    const { email, password } = req.body
+
+    try {
+
+        // verifica si el usuario existe
+        const usuario = await Usuario.findOne({ email })
+
+        if (!usuario) { // si el usuario existe True
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe'
+            })
+        }
+
+        // Confirmar password
+        const validPassword = bcrypt.compareSync(password, usuario.password)
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Contraseña incorrecta'
+            })
+        }
+
+        // Generar JWT
+        const token = await generarJWT(usuario.id, usuario.name)
+
+
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+
+        })
+
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
+
+}
+
+
+const revalidarToken = async (req, res = response) => {
+
+    const { uid, name } = req
+
+    const token = await generarJWT(uid, name)
+
+    res.json({
+        ok: true,
+        msg: 'renew',
+        token
+
+    })
+}
+
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+require('dotenv').config()
+const cors = require('cors')
+
+const dbConnection = require('./database/config');
+
+
+// crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection()
+
+// CORS
+app.use(cors())
+
+/// Directorio Público
+app.use(express.static('public'));
+
+// Lectura y parseo del body
+app.use(express.json());  // procesa las peticiones
+
+// Rutas
+//TODO: auth // crear, login, renew
+app.use('/api/auth', require('./routes/auth'));
+
+//TODO: CRUD: Eventos
+
+
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
+});
